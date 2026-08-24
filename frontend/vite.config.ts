38335ced440aea227c41ba46bb919b6d3d4244d8
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2026-08-24 17:01:10 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   server: {
+    allowedHosts: [
+      'sb-1zluefk7c5pa.vercel.run'
+    ]
+  }
   plugins: [react()],
   root: '.',
   build: { outDir: 'dist' },
